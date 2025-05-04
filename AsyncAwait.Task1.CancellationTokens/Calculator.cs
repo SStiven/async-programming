0316f435fb4d8df1d85bc10151c40aa98f821eb7
Stiven Villegas <stiven_villegas@epam.com>
2025-05-04 17:27:13 -0500

    Solve Task 1

---
+++ AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    public static Task<long> CalculateAsync(int n, CancellationToken token)
+    {
+        return Task.Run(() =>
+        {
+            token.ThrowIfCancellationRequested();
+
+            long sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                token.ThrowIfCancellationRequested();
+
+                sum = checked(sum + i);
+                Thread.Sleep(500);
+            }
+
+            return sum;
+        }, token);
+    }
+}
